ddb4116fda777794a36717733d9719722578f03b
Blair Cahue <blair.cahue@freedompay.com>
2019-11-17 22:31:22 -0500

    Try and trick mingw to not see sh.exe

--- appveyor_before_build.ps1
+++ appveyor_before_build.ps1
@@ -6,10 +6,10 @@ Start-FileDownload "https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer
 7z x "SDL2_mixer-devel-2.0.1-VC.zip"
 Start-FileDownload "https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.3/wxWidgets-3.1.3.zip"
 7z x "wxWidgets-3.1.3.zip" -o*
-& "C:\Program Files (x86)\cmake\bin\cmake.exe" .\wxWidgets-3.1.3 -G "MinGW Makefiles" -DwxBUILD_COMPATIBILITY="2.8" -DCMAKE_BUILD_TYPE=Release -DwxBUILD_SHARED=False
+& "C:\Program Files (x86)\cmake\bin\cmake.exe" .\wxWidgets-3.1.3 -G "MinGW Makefiles" -DwxBUILD_COMPATIBILITY="2.8" -DCMAKE_BUILD_TYPE=Release -DwxBUILD_SHARED=False -DCMAKE_SH="CMAKE_SH-NOTFOUND"
 & "C:\Program Files (x86)\cmake\bin\cmake.exe" --build .\wxWidgets-3.1.3
-& "C:\Program Files (x86)\cmake\bin\cmake.exe" .. -G "MinGW Makefiles" -DUSE_MINIUPNP=False -DSDL2_INCLUDE_DIR="./SDL2-2.0.5/include/" -DSDL2_LIBRARY="./SDL2-2.0.5/lib/x86/SDL2.lib" -DSDL2MAIN_LIBRARY="./lib/x86/SDL2main.lib" -DSDL2_MIXER_INCLUDE_DIR="./SDL2_mixer-2.0.1/include/" -DSDL2_MIXER_LIBRARY="./SDL2_mixer-2.0.1/lib/x86/SDL2_mixer.lib" -DUSE_STATIC_STDLIB=True -DwxWidgets_wxrc_EXECUTABLE="./wxWidgets-3.1.3/lib/gcc_lib/wxrc.exe"
-& "C:\Program Files (x86)\cmake\bin\cmake.exe" --build .\odalaunch
+& "C:\Program Files (x86)\cmake\bin\cmake.exe" .. -G "MinGW Makefiles" -DUSE_MINIUPNP=False -DSDL2_INCLUDE_DIR="./SDL2-2.0.5/include/" -DSDL2_LIBRARY="./SDL2-2.0.5/lib/x86/SDL2.lib" -DSDL2MAIN_LIBRARY="./lib/x86/SDL2main.lib" -DSDL2_MIXER_INCLUDE_DIR="./SDL2_mixer-2.0.1/include/" -DSDL2_MIXER_LIBRARY="./SDL2_mixer-2.0.1/lib/x86/SDL2_mixer.lib" -DUSE_STATIC_STDLIB=True -DwxWidgets_wxrc_EXECUTABLE="./wxWidgets-3.1.3/lib/gcc_lib/wxrc.exe" -DCMAKE_SH="CMAKE_SH-NOTFOUND"
+& "C:\Program Files (x86)\cmake\bin\cmake.exe" --build ..\odalaunch
 rm .\CMakeCache.txt
 & "C:\Program Files (x86)\cmake\bin\cmake.exe" .. -G "Visual Studio 14 2015" -DUSE_MINIUPNP=False -DSDL2_INCLUDE_DIR="./SDL2-2.0.5/include/" -DSDL2_LIBRARY="./SDL2-2.0.5/lib/x86/SDL2.lib" -DSDL2MAIN_LIBRARY="./lib/x86/SDL2main.lib" -DSDL2_MIXER_INCLUDE_DIR="./SDL2_mixer-2.0.1/include/" -DSDL2_MIXER_LIBRARY="./SDL2_mixer-2.0.1/lib/x86/SDL2_mixer.lib"
 cd ..
